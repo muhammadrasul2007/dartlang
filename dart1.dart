d3f967c69d9cab38da5de7360f0f1d6e239e7998
muhammadrasul2007 <124247331+muhammadrasul2007@users.noreply.github.com>
2023-02-15 20:37:43 +0500

    Create dart1.dart

---
+++ dart1.dart
@@ -0,0 +1,20 @@
+void main(){
+	print("Salom dunyo");
+	//Hello world
+	String FirstName = "Muhammadrasul";
+	String LastName = "Abdumutalibov";
+    String MiddleName = "Alijon o`ģli";
+    //Task1
+}
+//Task2
+void main(){
+	String Pdp = "Pdp Service";
+	printPdp();
+	//PdpService
+}
+//Task3
+void main(){
+	string first = "Muhammadrasul";
+	print("Hello" + $first);
+}
+
